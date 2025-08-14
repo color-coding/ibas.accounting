79246c7bfccdd2f83b2dbba9a16868436006fc10
Niuren.Zhu <niuren.zhu@icloud.com>
2025-08-14 16:12:13 +0800

    优化代码

--- ibas.accounting.service/src/main/webapp/bsapp/Console.ts
+++ ibas.accounting.service/src/main/webapp/bsapp/Console.ts
@@ -64,9 +64,9 @@ namespace accounting {
             /** 运行 */
             run(): void {
                 // 加载语言-框架默认
-                ibas.i18n.load([
-                    this.rootUrl + "resources/languages/accounting.json",
-                    this.rootUrl + "resources/languages/bos.json",
+                this.loadResources([
+                    "resources/languages/accounting.json",
+                    "resources/languages/bos.json",
                 ], () => {
                     // 设置资源属性
                     this.description = ibas.i18n.prop(this.name.toLowerCase());
